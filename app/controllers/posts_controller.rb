7bac1d624e2ee8f03c9377cd88d8b51103939b43
Szymon Wlochowski <szymon.wlochowski@onthebeach.co.uk>
2016-03-07 00:21:55 +0000

    Create & show posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,25 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to root_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
   end
 end
