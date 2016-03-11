a98ce8ac2d80de7c765ed61b65d8251841194ae1
Szymon Wlochowski <szymon.wlochowski@onthebeach.co.uk>
2016-03-11 16:11:06 +0000

    Alerts and refactoring.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -9,27 +10,34 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to root_path
+    if @post = Post.create(post_params)
+      flash[:success] = 'You have successfully created a new post.'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'An error occured. Your post has not been created.'
+      render :new
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    if @post.update(post_params)
+      flash[:success] = 'You have successfully updated your post.'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'An error occured. Your post has not been updated.'
+      render :edit
+    end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
+    flash[:success] = 'You have successfully deleted your post.'
     redirect_to root_path
   end
 
@@ -37,5 +45,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
