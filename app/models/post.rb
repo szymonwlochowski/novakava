32f53745205dbd0717721f1079dc83aa80f3be4e
Szymon Wlochowski <szymon.wlochowski@onthebeach.co.uk>
2016-03-18 15:14:43 +0000

    Review.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   validates :user_id, presence: true
   belongs_to :user
 
-  has_attached_file :image, styles: { :medium => "640px" }
+  has_attached_file :image, styles: { medium: "640px" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
