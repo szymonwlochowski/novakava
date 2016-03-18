a310ebf22c21663f56ce1afceb71e30acc8592e0
Szymon Wlochowski <szymon.wlochowski@onthebeach.co.uk>
2016-03-18 13:51:47 +0000

    Use devise for registrations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,4 +55,5 @@ Rails.application.routes.draw do
   #   end
 
   resources :posts
+  devise_for :users, :controllers => { registrations: 'registrations' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314152817) do
+ActiveRecord::Schema.define(version: 20160315164039) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -36,9 +36,11 @@ ActiveRecord::Schema.define(version: 20160314152817) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true
 
 end
