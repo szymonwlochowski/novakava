693619c0868cdcd8151e76d96406fcf407d92d86
Szymon Wlochowski <szymon.wlochowski@onthebeach.co.uk>
2016-03-18 15:08:36 +0000

    Protect posts from being edited by strangers.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -52,5 +53,12 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    if @post.user != current_user
+      flash[:alert] = "You can't do anything with this post, dumbo."
+      redirect_to root_path
+    end
   end
 end
