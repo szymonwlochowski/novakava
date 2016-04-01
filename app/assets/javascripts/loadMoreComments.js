ee88b67b4270088113c51f14cac114cc14e16667
Szymon Wlochowski <szymonwlochowski@gmail.com>
2016-04-01 21:24:01 +0100

    Bug fix. Now comments load properly.

--- app/assets/javascripts/loadMoreComments.js
+++ app/assets/javascripts/loadMoreComments.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  #('.more-comments').click( function() {
+  $('.more-comments').click( function() {
     $(this).on('ajax:success', function(event, data, status, xhr) {
       var postId = $(this).data("post-id");
       $("#comments_" + postId).html(data);
